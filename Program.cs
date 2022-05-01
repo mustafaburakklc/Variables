dcb41a268fb21a170126e5a1b6f91bf803990c7c
Mustafa Burak Kilic <burak.kilic@mobilistbilisim.com>
2022-05-01 20:18:42 +0300

    first commit

---
+++ Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+
+namespace Variables
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            DateTime dt= DateTime.Now;
+            Console.WriteLine(dt);
+            string dt2= DateTime.Now.ToString("dd.MM.yyyy");
+            string dt3= DateTime.Now.ToString("HH:mm:ss");
+            Console.WriteLine(dt2);
+            Console.WriteLine(dt3);
+
+            string a="20";
+             int number= int.Parse(a); //Parse function of int class that can make transform string values to int
+             string ad = number.ToString();
+             int number2=Convert.ToInt32(ad);
+
+             Console.WriteLine(number2);
+        }
+    }
+}
